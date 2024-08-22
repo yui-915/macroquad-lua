5c96da2ed907fe5e9ab07c6d7abc389ff343b533
Yui <yui300127@gmail.com>
2024-08-22 13:39:26 +0300

    fixed release macro including non-lua files

--- deps/macroquad-lua-macros/src/lib.rs
+++ deps/macroquad-lua-macros/src/lib.rs
@@ -1,28 +1,29 @@
 use quote::quote;
-use std::path::PathBuf;
 use std::path::Path;
+use std::path::PathBuf;
 use syn::{parse_macro_input, LitStr};
-// use quote::ToTokens;
 
 #[proc_macro]
 pub fn embed_lua_files(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-
     let base_path_str = parse_macro_input!(input as LitStr).value();
     let base_path = Path::new(&base_path_str);
     let mut parts: Vec<proc_macro2::TokenStream> = vec![];
 
-        for path in &list_all_paths(&base_path) {
-            let mut name = path
-                .iter()
-                .skip(base_path.iter().count())
-                .take(path.iter().count() - base_path.iter().count())
-                .map(|p| p.to_str().unwrap())
-                .collect::<Vec<&str>>()
-                .join(".");
-            name.pop();
-            name.pop();
-            name.pop();
-            name.pop();
+    for path in &list_all_paths(&base_path) {
+        let mut name = path
+            .iter()
+            .skip(base_path.iter().count())
+            .take(path.iter().count() - base_path.iter().count())
+            .map(|p| p.to_str().unwrap())
+            .collect::<Vec<&str>>()
+            .join(".");
+        name.pop();
+        name.pop();
+        name.pop();
+        name.pop();
+        if !name.ends_with(".lua") {
+            continue;
+        }
         let absolute_path = path.canonicalize().unwrap();
         let path_str = absolute_path.to_str().unwrap();
         parts.push(quote! {
@@ -39,7 +40,7 @@ pub fn embed_lua_files(input: proc_macro::TokenStream) -> proc_macro::TokenStrea
     .into()
 }
 
- fn list_all_paths(path: &Path) -> Vec<PathBuf> {
+fn list_all_paths(path: &Path) -> Vec<PathBuf> {
     let mut paths = vec![];
     for entry in path.read_dir().unwrap() {
         let entry = entry.unwrap();
